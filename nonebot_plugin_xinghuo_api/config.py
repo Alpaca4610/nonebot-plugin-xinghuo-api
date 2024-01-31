17a54d40b409f41c28d0a13243afa294d68c25ca
Alpaca <461065836@qq.com>
2024-01-31 12:22:02 +0800

    bug fix

--- nonebot_plugin_xinghuo_api/config.py
+++ nonebot_plugin_xinghuo_api/config.py
@@ -9,7 +9,7 @@ class Config(BaseModel, extra=Extra.ignore):
 
     xinghuo_enable_private_chat: bool = True # 私聊开关，默认开启，改为False关闭
     xinghuo_group_public: bool = False  # 群聊是否开启公共会话
-    xinghuo_api_version: str = "" #星火大模型的版本，默认为v1.5。使用2.0版本请填入v2，使用3.0版本请填入v3。使用助手API请输入助手id，在https://xinghuo.xfyun.cn/botcenter/createbot获取
+    xinghuo_api_version: str = "v2" #星火大模型的版本，默认为v1.5。使用2.0版本请填入v2，使用3.0版本请填入v3。使用助手API请输入助手id，在https://xinghuo.xfyun.cn/botcenter/createbot获取
 
 
 class ConfigError(Exception):
